ac7bd87ee6a8870b3b30354b49cfdb67b40d1367
yumcyawiz <hashdolf1923@gmail.com>
2021-09-18 18:21:58 +0900

    read ch8 binary

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 mod chip8;
 mod opcode;
 
+use std::fs::File;
+use std::io::prelude::*;
+
 #[macro_use]
 extern crate lazy_static;
 
 fn main() {
-    println!("Hello, world!");
+    let mut f = File::open("test_opcode.ch8").expect("Failed to open the file");
+
+    let mut program: Vec<u8> = Vec::new();
+    f.read_to_end(&mut program)
+        .expect("failed to read the file");
 }
