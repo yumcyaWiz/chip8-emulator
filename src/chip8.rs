6a59fafa469446589529b4a7db40a8aae3c1a0eb
yumcyawiz <hashdolf1923@gmail.com>
2021-09-21 18:08:41 +0900

    fix info! format

--- src/chip8.rs
+++ src/chip8.rs
@@ -12,7 +12,7 @@ pub struct Chip8 {
 
     memory: [u8; 0x1000],
 
-    display: [bool; 64 * 32],
+    pub display: [bool; 64 * 32],
 
     delay_timer: u8,
     sound_timer: u8,
@@ -138,7 +138,7 @@ impl Chip8 {
             */
 
             // fetch opcode
-            let current_index = self.program_counter;
+            let program_index = self.program_counter;
             let opcode = self.read_memory_u16(self.program_counter);
             self.program_counter += 2;
 
@@ -147,12 +147,16 @@ impl Chip8 {
                 0x0000 => match opcode {
                     0x00E0 => {
                         // CLS
+                        info!("{:X}, CLS", program_index);
+
                         for v in self.display.iter_mut() {
                             *v = false;
                         }
                     }
                     0x00EE => {
                         // RET
+                        info!("{:X}, RET", program_index);
+
                         self.program_counter = self.pop();
                     }
                     _ => {
@@ -162,14 +166,14 @@ impl Chip8 {
                 0x1000 => {
                     // JP addr
                     let nnn = opcode & 0x0FFF;
-                    info!("{:X}: JP, {:X}", current_index, nnn);
+                    info!("{:X}: JP, {:X}", program_index, nnn);
 
                     self.program_counter = nnn;
                 }
                 0x2000 => {
                     // CALL addr
                     let nnn = opcode & 0x0FFF;
-                    info!("{:X}: CALL, {:X}", current_index, nnn);
+                    info!("{:X}: CALL, {:X}", program_index, nnn);
 
                     self.push(self.program_counter);
                     self.program_counter = nnn;
@@ -178,7 +182,7 @@ impl Chip8 {
                     // SE Vx, byte
                     let x = (opcode & 0x0F00 >> 8) as u8;
                     let kk = (opcode & 0x00FF) as u8;
-                    info!("{:X}: SE, V{} {:X}", current_index, x, kk);
+                    info!("{:X}: SE, V{} {:X}", program_index, x, kk);
 
                     if self.read_register(x) == kk {
                         self.program_counter += 2;
@@ -188,7 +192,7 @@ impl Chip8 {
                     // SNE Vx, byte
                     let x = (opcode & 0x0F00 >> 8) as u8;
                     let kk = (opcode & 0x00FF) as u8;
-                    info!("{:X}: SNE, V{} {:X}", current_index, x, kk);
+                    info!("{:X}: SNE, V{} {:X}", program_index, x, kk);
 
                     if self.read_register(x) != kk {
                         self.program_counter += 2;
@@ -200,7 +204,7 @@ impl Chip8 {
                             // SE Vx, Vy
                             let x = (opcode & 0x0F00 >> 8) as u8;
                             let y = (opcode & 0x00F0 >> 4) as u8;
-                            info!("{:X}: SE, V{} V{}", current_index, x, y);
+                            info!("{:X}: SE, V{} V{}", program_index, x, y);
 
                             if self.read_register(x) == self.read_register(y) {
                                 self.program_counter += 2;
@@ -213,7 +217,7 @@ impl Chip8 {
                     // LD Vx, byte
                     let x = (opcode & 0x0F00 >> 8) as u8;
                     let kk = (opcode & 0x00FF) as u8;
-                    info!("{:X}: LD, V{}, {:X}", current_index, x, kk);
+                    info!("{:X}: LD, V{}, {:X}", program_index, x, kk);
 
                     self.write_register(x, kk);
                 }
@@ -221,7 +225,7 @@ impl Chip8 {
                     // ADD Vx, byte
                     let x = (opcode & 0x0F00 >> 8) as u8;
                     let kk = (opcode & 0x00FF) as u8;
-                    info!("{:X}: ADD, V{}, {:X}", current_index, x, kk);
+                    info!("{:X}: ADD, V{}, {:X}", program_index, x, kk);
 
                     self.write_register(x, self.read_register(x).wrapping_add(kk));
                 }
@@ -230,7 +234,7 @@ impl Chip8 {
                         // LD Vx, Vy
                         let x = (opcode & 0x0F00 >> 8) as u8;
                         let y = (opcode & 0x00F0 >> 4) as u8;
-                        info!("{:X}: LD, V{}, V{}", current_index, x, y);
+                        info!("{:X}: LD, V{}, V{}", program_index, x, y);
 
                         self.write_register(x, self.read_register(y));
                     }
@@ -238,7 +242,7 @@ impl Chip8 {
                         // OR Vx, Vy
                         let x = (opcode & 0x0F00 >> 8) as u8;
                         let y = (opcode & 0x00F0 >> 4) as u8;
-                        info!("{:X}: OR, V{}, V{}", current_index, x, y);
+                        info!("{:X}: OR, V{}, V{}", program_index, x, y);
 
                         self.write_register(x, self.read_register(x) | self.read_register(y));
                     }
@@ -246,7 +250,7 @@ impl Chip8 {
                         // AND Vx, Vy
                         let x = (opcode & 0x0F00 >> 8) as u8;
                         let y = (opcode & 0x00F0 >> 4) as u8;
-                        info!("{:X}: AND, V{}, V{}", current_index, x, y);
+                        info!("{:X}: AND, V{}, V{}", program_index, x, y);
 
                         self.write_register(x, self.read_register(x) & self.read_register(y));
                     }
@@ -254,7 +258,7 @@ impl Chip8 {
                         // XOR Vx, Vy
                         let x = (opcode & 0x0F00 >> 8) as u8;
                         let y = (opcode & 0x00F0 >> 4) as u8;
-                        info!("{:X}: XOR, V{}, V{}", current_index, x, y);
+                        info!("{:X}: XOR, V{}, V{}", program_index, x, y);
 
                         self.write_register(x, self.read_register(x) ^ self.read_register(y));
                     }
@@ -262,7 +266,7 @@ impl Chip8 {
                         // ADD Vx, Vy
                         let x = (opcode & 0x0F00 >> 8) as u8;
                         let y = (opcode & 0x00F0 >> 4) as u8;
-                        info!("{:X}: ADD, V{}, V{}", current_index, x, y);
+                        info!("{:X}: ADD, V{}, V{}", program_index, x, y);
 
                         self.write_register(x, self.read_register(x) + self.read_register(y));
                     }
@@ -270,7 +274,7 @@ impl Chip8 {
                         // SUB Vx, Vy
                         let x = (opcode & 0x0F00 >> 8) as u8;
                         let y = (opcode & 0x00F0 >> 4) as u8;
-                        info!("{:X}: SUB, V{}, V{}", current_index, x, y);
+                        info!("{:X}: SUB, V{}, V{}", program_index, x, y);
 
                         self.write_register(x, self.read_register(x) - self.read_register(y));
                     }
@@ -278,7 +282,7 @@ impl Chip8 {
                         // SHR Vx, Vy
                         let x = (opcode & 0x0F00 >> 8) as u8;
                         let y = (opcode & 0x00F0 >> 4) as u8;
-                        info!("{:X}: SHR, V{}, V{}", current_index, x, y);
+                        info!("{:X}: SHR, V{}, V{}", program_index, x, y);
 
                         self.write_register(0xF, x & 0b0000_0001);
                         self.write_register(x, self.read_register(y) >> 1);
@@ -289,7 +293,7 @@ impl Chip8 {
                         let y = (opcode & 0x00F0 >> 4) as u8;
                         let vx = self.read_register(x);
                         let vy = self.read_register(y);
-                        info!("{:X}: SUBN, V{}, V{}", current_index, x, y);
+                        info!("{:X}: SUBN, V{}, V{}", program_index, x, y);
 
                         self.write_register(0xF, if vy > vx { 1 } else { 0 });
                         self.write_register(x, vy - vx);
@@ -298,7 +302,7 @@ impl Chip8 {
                         // SHL Vx, Vy
                         let x = (opcode & 0x0F00 >> 8) as u8;
                         let y = (opcode & 0x00F0 >> 4) as u8;
-                        info!("{:X}: SHL, V{}, V{}", current_index, x, y);
+                        info!("{:X}: SHL, V{}, V{}", program_index, x, y);
 
                         self.write_register(0xF, x & 0b1000_0000);
                         self.write_register(x, self.read_register(y) << 1);
@@ -311,7 +315,7 @@ impl Chip8 {
                             // SNE Vx, Vy
                             let x = (opcode & 0x0F00 >> 8) as u8;
                             let y = (opcode & 0x00F0 >> 4) as u8;
-                            info!("{:X}: SNE, V{}, V{}", current_index, x, y);
+                            info!("{:X}: SNE, V{}, V{}", program_index, x, y);
 
                             if self.read_register(x) != self.read_register(y) {
                                 self.program_counter += 2;
@@ -323,17 +327,14 @@ impl Chip8 {
                 0xA000 => {
                     // LD I, addr
                     let nnn = opcode & 0x0FFF;
-                    info!(
-                        "{:X}: LD, {:X}, {:X}",
-                        current_index, self.index_register, nnn
-                    );
+                    info!("{:X}: LD, I, {:X}", program_index, nnn);
 
                     self.index_register = nnn;
                 }
                 0xB000 => {
                     // JP V0, addr
                     let nnn = opcode & 0x0FFF;
-                    info!("{:X}: JP, V0, {:X}", current_index, nnn);
+                    info!("{:X}: JP, V0, {:X}", program_index, nnn);
 
                     self.program_counter = (self.read_register(0) as u16) + nnn;
                 }
@@ -341,7 +342,7 @@ impl Chip8 {
                     // RND Vx, byte
                     let x = (opcode & 0x0F00 >> 8) as u8;
                     let kk = (opcode & 0x00FF) as u8;
-                    info!("{:X}: RND, V{}, {:X}", current_index, x, kk);
+                    info!("{:X}: RND, V{}, {:X}", program_index, x, kk);
 
                     let rnd: u8 = self.rng.gen_range(0..255);
                     self.write_register(x, rnd & kk);
@@ -351,7 +352,7 @@ impl Chip8 {
                     let x = (opcode & 0x0F00 >> 8) as u8;
                     let y = (opcode & 0x00F0 >> 4) as u8;
                     let n = (opcode & 0x000F) as u8;
-                    info!("{:X}: DRW, V{}, V{}, {:b}", current_index, x, y, n);
+                    info!("{:X}: DRW, V{}, V{}, {:b}", program_index, x, y, n);
 
                     // draw
                     let mut erased = false;
@@ -374,7 +375,7 @@ impl Chip8 {
                     0xE09E => {
                         // SKP Vx
                         let x = (opcode & 0x0F00 >> 8) as u8;
-                        info!("{:X}: SKP, V{}", current_index, x);
+                        info!("{:X}: SKP, V{}", program_index, x);
 
                         if self.read_keyboard(x) {
                             self.program_counter += 2;
@@ -383,7 +384,7 @@ impl Chip8 {
                     0xE0A1 => {
                         // SKNP Vx
                         let x = (opcode & 0x0F00 >> 8) as u8;
-                        info!("{:X}: SKNP, V{}", current_index, x);
+                        info!("{:X}: SKNP, V{}", program_index, x);
 
                         if !self.read_keyboard(x) {
                             self.program_counter += 2;
@@ -395,14 +396,14 @@ impl Chip8 {
                     0xF007 => {
                         // LD Vx, DT
                         let x = (opcode & 0x0F00 >> 8) as u8;
-                        info!("{:X}: LD, V{}, DT", current_index, x);
+                        info!("{:X}: LD, V{}, DT", program_index, x);
 
                         self.write_register(x, self.delay_timer);
                     }
                     0xF00A => {
                         // LD Vx, K
                         let x = (opcode & 0x0F00 >> 8) as u8;
-                        info!("{:X}: LD, V{}, K", current_index, x);
+                        info!("{:X}: LD, V{}, K", program_index, x);
 
                         // wait until any key pressed
                         let mut key_index = 0;
@@ -425,14 +426,14 @@ impl Chip8 {
                     0xF015 => {
                         // LD DT, Vx
                         let x = (opcode & 0x0F00 >> 8) as u8;
-                        info!("{:X}: LD, DT, V{}", current_index, x);
+                        info!("{:X}: LD, DT, V{}", program_index, x);
 
                         self.delay_timer = self.read_register(x);
                     }
                     0xF018 => {
                         // LD ST, Vx
                         let x = (opcode & 0x0F00 >> 8) as u8;
-                        info!("{:X}: LD, ST, V{}", current_index, x);
+                        info!("{:X}: LD, ST, V{}", program_index, x);
 
                         self.sound_timer = self.read_register(x);
                     }
@@ -441,7 +442,7 @@ impl Chip8 {
                         let x = (opcode & 0x0F00 >> 8) as u8;
                         info!(
                             "{:X}: ADD, {:X}, V{}",
-                            current_index, self.index_register, x
+                            program_index, self.index_register, x
                         );
 
                         self.index_register += self.read_register(x) as u16;
@@ -452,7 +453,7 @@ impl Chip8 {
                     0xF033 => {
                         // LD B, Vx
                         let x = (opcode & 0x0F00 >> 8) as u8;
-                        info!("{:X}: LD, B, V{}", current_index, x);
+                        info!("{:X}: LD, B, V{}", program_index, x);
 
                         let vx = self.read_register(x);
                         let hundred = (vx / 100) % 10;
@@ -468,7 +469,7 @@ impl Chip8 {
                         let x = (opcode & 0x0F00 >> 8) as u8;
                         info!(
                             "{:X}: LD, [{:X}], V{}",
-                            current_index, self.index_register, x
+                            program_index, self.index_register, x
                         );
 
                         for i in 0..x {
@@ -483,7 +484,7 @@ impl Chip8 {
                         let x = (opcode & 0x0F00 >> 8) as u8;
                         info!(
                             "{:X}: LD, V{}, [{:X}]",
-                            current_index, x, self.index_register
+                            program_index, x, self.index_register
                         );
 
                         for i in 0..x {
